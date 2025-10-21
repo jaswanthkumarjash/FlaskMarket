45534d25ea92b62c08da82df238b1fcadaf9799a
jaswanth kumar <jaswanthkumarjash2000@gmail.com>
2025-10-21 16:35:19 +0530

    Sending Data to templates

--- market.py
+++ market.py
@@ -7,3 +7,11 @@ app = Flask(__name__)
 def home_page():
     return render_template("home.html")
 
+@app.route("/market")
+def maarket_page():
+    items = [
+        {'id': 1, 'name': 'phone','barcode' : '893211235466', 'price': 1000},
+        {'id': 2, 'name': 'laptop','barcode' : '893211235467', 'price': 2000},
+        {'id': 3, 'name': 'tablet','barcode' : '893211235468', 'price': 1500}
+    ]
+    return render_template("market.html", items=items)
